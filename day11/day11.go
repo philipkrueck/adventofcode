983b4c12e93e9b40895d48aa137ecb860a689b84
Philip Krück <hi@philipkrueck.com>
2025-12-30 09:53:37 -0300

    feat: solution day 11 part 2

--- day11/day11.go
+++ day11/day11.go
@@ -10,16 +10,19 @@ func Part1() int {
 	r := lines.NewReader("day11/input.txt")
 	lines := r.Lines()
 	conns := parse(lines)
+	cache := make(map[string]int)
 
-	return countPaths(conns)
+	return countPaths(conns, "you", cache)
 }
 
 func Part2() int {
 	r := lines.NewReader("day11/input.txt")
 	lines := r.Lines()
 	conns := parse(lines)
+	start := Node{"svr", false, false}
+	cache := make(map[Node]int)
 
-	return len(conns) * 0
+	return countPathsPart2(conns, start, cache)
 }
 
 func parse(lines []string) map[string][]string {
@@ -41,22 +44,51 @@ func parseLine(line string) (string, []string) {
 	return device, outputs
 }
 
-func countPaths(nodes map[string][]string) int {
-	var count int
+type Node struct {
+	node     string
+	fft, dac bool
+}
 
-	queue := []string{"you"}
-
-	for len(queue) > 0 {
-		node := queue[0]
-		queue = queue[1:]
-
-		if node == "out" {
-			count++
-			continue
-		}
-
-		queue = append(queue, nodes[node]...)
+func countPaths(nodes map[string][]string, node string, cache map[string]int) int {
+	if node == "out" {
+		return 1
 	}
 
+	if count, ok := cache[node]; ok {
+		return count
+	}
+
+	var count int
+	for _, neighbor := range nodes[node] {
+		count += countPaths(nodes, neighbor, cache)
+	}
+
+	cache[node] = count
+	return count
+}
+
+func countPathsPart2(nodes map[string][]string, node Node, cache map[Node]int) int {
+	switch node.node {
+	case "out":
+		if node.dac && node.fft {
+			return 1
+		}
+	case "dac":
+		node.dac = true
+	case "fft":
+		node.fft = true
+	}
+
+	if count, ok := cache[node]; ok {
+		return count
+	}
+
+	var count int
+	for _, neighbor := range nodes[node.node] {
+		neighborNode := Node{neighbor, node.fft, node.dac}
+		count += countPathsPart2(nodes, neighborNode, cache)
+	}
+
+	cache[node] = count
 	return count
 }
