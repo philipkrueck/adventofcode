17094e3c2ef8d31bd1d3a69d80e35a33e89acb70
Philip Krück <hi@philipkrueck.com>
2026-02-28 10:44:59 -0300

    refactor 2015 day2 Part 1

--- 2015/day02/solution.go
+++ 2015/day02/solution.go
@@ -3,8 +3,10 @@ package day02
 
 import (
 	_ "embed"
-	"fmt"
+	"strconv"
+	"strings"
 
+	"github.com/philipkrueck/advent-of-code/internal/parse"
 	"github.com/philipkrueck/advent-of-code/internal/registry"
 )
 
@@ -12,11 +14,38 @@ import (
 var rawInput string
 
 func Part1(input string) string {
-	return fmt.Sprintf("%d", len(input))
+	sum := 0
+	for _, present := range parse.Lines(input) {
+		l, w, h := parseLines(present)
+		s1, s2, s3 := l*w, w*h, l*h
+		area := 2*s1 + 2*s2 + 2*s3
+		smallest := min(s1, s2, s3)
+		sum += area + smallest
+	}
+
+	return strconv.Itoa(sum)
 }
 
 func Part2(input string) string {
-	return fmt.Sprintf("%d", len(input))
+	return strconv.Itoa(len(input))
+}
+
+func parseLines(line string) (int, int, int) {
+	parts := strings.Split(line, "x")
+
+	l, err := strconv.Atoi(parts[0])
+	if err != nil {
+		panic(err)
+	}
+	w, err := strconv.Atoi(parts[1])
+	if err != nil {
+		panic(err)
+	}
+	h, err := strconv.Atoi(parts[2])
+	if err != nil {
+		panic(err)
+	}
+	return l, w, h
 }
 
 func init() {
--- 2015/day02/solution_test.go
+++ 2015/day02/solution_test.go
@@ -3,4 +3,42 @@ package day02
 import "testing"
 
 func TestPart1(t *testing.T) {
+	tests := []struct {
+		in, want string
+	}{
+		{"2x3x4", "58"},
+		{"1x1x10", "43"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			if got := Part1(tt.in); got != tt.want {
+				t.Errorf("got: %q; want: %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParsePresent(t *testing.T) {
+	tests := []struct {
+		in                  string
+		wantL, wantW, wantH int
+	}{
+		{
+			"2x3x4",
+			2, 3, 4,
+		},
+		{
+			"1x1x10",
+			1, 1, 10,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			gotL, gotW, gotH := parseLines(tt.in)
+			if gotL != tt.wantL || gotW != tt.wantW || gotH != tt.wantH {
+				t.Errorf("Got: (%d,%d,%d); Want: (%d,%d,%d)", gotL, gotW, gotH, tt.wantL, tt.wantW, tt.wantH)
+			}
+		})
+	}
 }
