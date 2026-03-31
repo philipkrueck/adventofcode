580621d5fb203fa918c730313e1b28a6fecbab4c
Philip Krück <hi@philipkrueck.com>
2026-03-31 23:47:04 +0100

    feat: add day2 2023

--- 2023/day02/solution.go
+++ 2023/day02/solution.go
@@ -3,7 +3,10 @@ package day02
 
 import (
 	_ "embed"
+	"strconv"
+	"strings"
 
+	"github.com/philipkrueck/adventofcode/internal/parse"
 	"github.com/philipkrueck/adventofcode/internal/registry"
 )
 
@@ -16,10 +19,102 @@ func init() {
 	registry.Register(year, day, 2, Part2, rawInput)
 }
 
+type game struct {
+	id      int
+	subsets []subset
+}
+
+type subset struct {
+	red, green, blue int
+}
+
+const maxRed, maxGreen, maxBlue = 12, 13, 14
+
 func Part1(input string) string {
-	return "1"
+	sum := 0
+
+	for line := range parse.LinesSeq(input) {
+		game := parseGame(line)
+		if game.isValid() {
+			sum += game.id
+		}
+
+	}
+
+	return strconv.Itoa(sum)
 }
 
 func Part2(input string) string {
-	return ""
+	sum := 0
+
+	for line := range parse.LinesSeq(input) {
+		game := parseGame(line)
+		sum += game.minCubesPower()
+	}
+
+	return strconv.Itoa(sum)
+}
+
+func (game game) minCubesPower() int {
+	var minBlue, minRed, minGreen int
+
+	for _, subset := range game.subsets {
+		minBlue = max(minBlue, subset.blue)
+		minRed = max(minRed, subset.red)
+		minGreen = max(minGreen, subset.green)
+	}
+
+	return minBlue * minRed * minGreen
+}
+
+func (game game) isValid() bool {
+	for _, subset := range game.subsets {
+		if subset.blue > maxBlue || subset.green > maxGreen || subset.red > maxRed {
+			return false
+		}
+	}
+	return true
+}
+
+func parseGame(line string) game {
+	var game game
+
+	colon := strings.IndexByte(line, ':')
+	id, err := strconv.Atoi(line[5:colon])
+	if err != nil {
+		panic(err)
+	}
+	game.id = id
+
+	subsetsStrs := strings.Split(line[colon+2:], "; ")
+	game.subsets = make([]subset, 0, len(subsetsStrs))
+
+	for _, subsetStr := range subsetsStrs {
+		var subset subset
+		cubes := strings.SplitSeq(subsetStr, ", ")
+		for cube := range cubes {
+			numStr, color, ok := strings.Cut(cube, " ")
+			if !ok {
+				panic("should find num and color")
+			}
+
+			num, err := strconv.Atoi(numStr)
+			if err != nil {
+				panic(err)
+			}
+
+			switch color {
+			case "red":
+				subset.red = num
+			case "green":
+				subset.green = num
+			case "blue":
+				subset.blue = num
+			}
+
+		}
+		game.subsets = append(game.subsets, subset)
+	}
+
+	return game
 }
--- 2023/day02/solution_test.go
+++ 2023/day02/solution_test.go
@@ -2,11 +2,17 @@ package day02
 
 import "testing"
 
+var in = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		in, want string
 	}{
-		{"", "1"},
+		{in, "8"},
 	}
 
 	for _, tt := range tests {
@@ -19,11 +25,10 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	t.Skip("skipping...")
 	tests := []struct {
 		in, want string
 	}{
-		{"", ""},
+		{in, "2286"},
 	}
 
 	for _, tt := range tests {
